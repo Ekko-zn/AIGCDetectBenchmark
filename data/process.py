0388d4346a16044f83497edab56bc86cd687f905
Yiran Xu (徐怡然) <68043278+Aemilia-xu@users.noreply.github.com>
2024-02-27 11:39:06 +0800

    Update process.py

--- data/process.py
+++ data/process.py
@@ -203,11 +203,7 @@ def processing(img, opt, name):
         rz_func = transforms.Lambda(lambda img: custom_resize(img, opt))
     trans = transforms.Compose([
                 rz_func,
-<<<<<<< Updated upstream
-                transforms.Lambda(lambda img: data_augment(img, opt) if opt.isTrain else img),
-=======
                 transforms.Lambda(lambda img: data_augment(img, opt) if (opt.isTrain or opt.isVal) else img),
->>>>>>> Stashed changes
                 crop_func,
                 flip_func,
                 transforms.ToTensor(),
